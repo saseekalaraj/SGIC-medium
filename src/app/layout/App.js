837ce5b382455f4ff2493394ffe4cdc305769259
unknown <sasee9488@gmail.com>
2019-10-13 22:58:40 +0530

    update more componets

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -2,17 +2,34 @@ import React, { Component, Fragment } from 'react';
 import EventDashboard from '../../features/event/eventDashborad/EventDashboard';
 import NavBar from '../../features/nav/NavBar/navBar';
 import { Container } from 'semantic-ui-react';
+import { Route } from 'react-router';
+import HomePage from '../../features/home/HomePage'
+import EventDetailedPage from '../../features/event/EventDetailed/EventDetailed'
+import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard'
+import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage'
+import SettingDashboard from '../../features/user/Settings/SettingDashboard'
+import EventForm from '../../features/event/EventFrom/EventForm'
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <NavBar />
-        <Container className="main">
-          <EventDashboard />
-        </Container>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/(.+)' render={() => (
+          <Fragment>
+            <NavBar />
+            <Container className="main">
+              <Route path='/events' component={EventDashboard} />
+              <Route path='/events/:id' component={EventDetailedPage} />
+              <Route path='/people' component={PeopleDashboard} />
+              <Route path='/profile/:id' component={UserDetailedPage} />
+              <Route path='/settings' component={SettingDashboard} />
+              <Route path='/createEvent' component={EventForm} />
+            </Container>
+          </Fragment>
+        )}/>
       </Fragment>
-    );
+      );
+    }
   }
-}
-export default App;
+  export default App;
---
+++ src/features/event/EventDetailed/EventDetailed.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EventDetailed = () => {
+    return (
+        <div>
+            <h1>event details</h1>
+        </div>
+    )
+}
+
+export default EventDetailed
