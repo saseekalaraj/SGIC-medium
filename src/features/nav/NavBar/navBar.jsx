afc8f42d9c9ace2a6cad40987322d796f24a6703
unknown <sasee9488@gmail.com>
2019-10-15 21:52:05 +0530

    update

--- src/features/nav/NavBar/navBar.jsx
+++ src/features/nav/NavBar/navBar.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 import { Menu, Container, Button } from 'semantic-ui-react'
-import { NavLink, Link } from 'react-router-dom'
+import { NavLink, Link, withRouter } from 'react-router-dom'
+import SignedOutMenu from '../Menus/SignedOutMenu'
+import SignedInMenu from '../Menus/SignedInMenu'
 
-export default class navBar extends Component {
+class NavBar extends Component {
+    state = {
+        authenticated: false
+    }
+    handleSignIn = () => this.setState({ authenticated: true })
+    handleSignOut = () => {
+        this.setState({ authenticated: false })
+        this.props.history.push('/')
+    }
     render() {
+        const { authenticated } = this.state
         return (
             <Menu inverted fixed="top">
                 <Container>
@@ -16,12 +27,10 @@ export default class navBar extends Component {
                     <Menu.Item>
                         <Button as={Link} to='/createEvent' floated="right" positive inverted content="Create Event" />
                     </Menu.Item>
-                    <Menu.Item position="right">
-                        <Button basic inverted content="Login" />
-                        <Button basic inverted content="Sign Out" style={{ marginLeft: '0.5em' }} />
-                    </Menu.Item>
+                    {authenticated ? <SignedInMenu signOut={this.handleSignOut} /> : <SignedOutMenu signIn={this.handleSignIn} />}
                 </Container>
             </Menu>
         )
     }
 }
+export default withRouter(NavBar)
--- src/features/user/Settings/SettingDashboard.jsx
+++ src/features/user/Settings/SettingDashboard.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Grid } from 'semantic-ui-react'
+import SettingNav from './SettingNav'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import BasicPage from './BasicPage'
+import AboutPage from './AboutPage'
+import PhotoPage from './PhotoPage'
+import AccountPage from './AccountPage'
+
 
 const SettingDashboard = () => {
     return (
-        <div>
-            <h1>setting</h1>
-        </div>
+        <Grid>
+            <Grid.Column width={12}>
+                <Switch>
+                    <Redirect exact from='/settings' to='/settings/basic' />
+                    <Route path='/settings/basic' component={BasicPage} />
+                    <Route path='/settings/about' component={AboutPage} />
+                    <Route path='/settings/photos' component={PhotoPage} />
+                    <Route path='/settings/account' component={AccountPage} />
+                </Switch>
+            </Grid.Column>
+            <Grid.Column width={4}>
+                <SettingNav />
+            </Grid.Column>
+        </Grid>
     )
 }
 
